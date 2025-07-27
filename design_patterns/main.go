d1752feb0c1b38a1c6ed8377ceef71a66a75e3e5
Jammedat <susilkhadka503@gmail.com>
2025-07-27 22:51:50 +0545

    initial commit

---
+++ design_patterns/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "packages/decorator"
+
+// "packages/factory"
+// "packages/observer"
+// "packages/singleton"
+
+func main() {
+	// singleton.SingletonPattern()
+	// factory.FactoryPattern()
+	// observer.ObserverPattern()
+	decorator.DecoratorPattern()
+}
